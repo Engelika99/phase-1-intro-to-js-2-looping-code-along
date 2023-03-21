cb40c967c77f092ad5d464c1c9df9e233c7935cc
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-03-20 23:06:09 -0400

    Uses for and while loops

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+    function writeCards(names, value){
+        let messageNames = [];
+        for(let i =0; i < names.length; i++){
+        messageNames.push(`Thank you, ${names[i]}, for the wonderful ${value} gift!`);
+}
+return messageNames;
+}
+    function countDown(number){
+        let value = number;
+        while(value >= 0){
+        console.log(value--);
+    }
+return number;
+}
